ac9dac7fdb5c9371b4498d2038cf9569567f6fc1
JhonezTI <contato.jhonezti@gmail.com>
2020-12-08 18:36:40 +0000

    Atualizar arquivo Routes e configurar

--- marketshop/routes.py
+++ marketshop/routes.py
@@ -6,6 +6,27 @@ from flask import render_template, redirect, url_for, request
 def index_pag():
     return render_template('Index.html')
 
-@app.route('/Home')
+@app.route('/Feedback')
+def feed_pag():
+    return render_template('Feed.html')
+
+@app.route('/Welcome')
 def home_pag():
     return render_template('Home.html')
+
+@app.route('/Perfil_Autonomo')
+def PerfilAUT_pag():
+    return render_template('PerfilSegundario.html')
+
+@app.route('/Recovery')
+def Recovery_pag():
+    return render_template('Recovery.html')
+
+@app.route('/Sign_In')
+def Sign_In():
+    return render_template('Sign_In.html')
+
+@app.route('/Sign_Up')
+def Sign_Up():
+    return render_template('Sign_In.html')
+
